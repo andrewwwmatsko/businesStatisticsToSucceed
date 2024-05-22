f7e583d6adb7fb4500ec4a6c4943c1d3331a2342
andrewwwmatsko <matskoandrew@gmail.com>
2024-05-22 15:56:38 +0300

    faqs section mob + js accordion

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// accordion
+const accordionWrapper = document.querySelectorAll(".faqs-item-title-wrapper");
+
+for (let i = 0; i < accordionWrapper.length; i++) {
+  accordionWrapper[i].addEventListener("click", (e) => {
+    const content = e.currentTarget.nextElementSibling;
+
+    const btn = e.currentTarget.querySelector(".show-more-btn");
+
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+      content.classList.remove("is-active");
+    } else {
+      content.style.maxHeight = `${content.scrollHeight}px`;
+      content.classList.add("is-active");
+    }
+  });
+}
